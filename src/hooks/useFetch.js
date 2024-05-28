4497cb19ecc9d263d645aa693be7197295e9adee
Kishan kumar <109970949+Jkishan544@users.noreply.github.com>
2024-05-28 13:50:00 +0530

    Add files via upload

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,27 @@
+import { useState, useEffect  } from "react"
+
+export const useFetch =  (url) => {
+ const [data,setData] = useState(null);
+ const[loading, setLoading] =useState(false);
+
+ useEffect(() =>{
+
+    const fetchData = async () =>{
+        setLoading(true);
+
+        try{
+     const response =await fetch(url);
+     const result = await response.json();
+     setLoading(false);
+     setData(result);
+        }
+        catch(error){
+             
+        }
+    }
+    fetchData();
+},[url]);
+ 
+ 
+    return{data, loading }
+}
